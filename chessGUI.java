ba061f560e7acce1a6b7394d2704bf195184f313
1 <aarons114@nycstudents.net>
2022-05-18 10:45:36 -0400

    1

--- chessGUI.java
+++ chessGUI.java
@@ -38,7 +38,19 @@ public class chessGUI extends JFrame implements EventListener{
         chessBoard = new JPanel(new GridLayout(8,8));
         chessBoard.setBorder(new LineBorder(Color.BLACK));
         gui.add(chessBoard);
-        ImageIcon icon = new ImageIcon("images/blackBishop.PNG");
+        ImageIcon bbish = new ImageIcon("images/blackBishop.PNG");
+        ImageIcon brook = new ImageIcon("images/blackRook.PNG");
+        ImageIcon bking = new ImageIcon("images/blackKing.PNG");
+        ImageIcon bqueen = new ImageIcon("images/blackQueen.PNG");
+        ImageIcon bpawn = new ImageIcon("images/blackPawn.PNG");
+        ImageIcon bknight = new ImageIcon("images/blackKnight.PNG");
+        ImageIcon wbish = new ImageIcon("images/whiteBishop.PNG");
+        ImageIcon wrook = new ImageIcon("images/whiteRook.PNG");
+        ImageIcon wking = new ImageIcon("images/whiteKing.PNG");
+        ImageIcon wqueen = new ImageIcon("images/whiteQueen.PNG");
+        ImageIcon wpawn = new ImageIcon("images/whitePawn.PNG");
+        ImageIcon wknight = new ImageIcon("images/whiteKnight.PNG");
+
 
         Insets buttonMargins = new Insets(0,0,0,0);
         for (int row = 0; row < chessBoardSquares.length; row++)
@@ -52,23 +64,35 @@ public class chessGUI extends JFrame implements EventListener{
             }
             for (int col = 0; col < chessBoardSquares[0].length; col++)
             {
-                chessBoardSquares[row][col] = new JButton(icon);
+                chessBoardSquares[row][col] = new JButton();
                 chessBoardSquares[row][col].setMargin(buttonMargins);
-//                ImageIcon icon = new ImageIcon(
-//                        new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB));
-                chessBoardSquares[row][col].setIcon(icon);
 
-//                if (black){
-//                    chessBoardSquares[row][col].setBackground(Color.BLACK);
-//                    black = false;
-//                }
-//                else{
-//                    chessBoardSquares[row][col].setBackground(Color.WHITE);
-//                    black = true;
-//                }
+                if (black){
+                    chessBoardSquares[row][col].setBackground(Color.BLACK);
+                    black = false;
+                }
+                else{
+                    chessBoardSquares[row][col].setBackground(Color.WHITE);
+                    black = true;
+                }
                 chessBoard.add(chessBoardSquares[row][col]);
             }
         }
+        for(int i = 0;i<8;i++){
+            chessBoardSquares[1][i].setIcon(bpawn);
+        }
+        for(int i = 0;i<8;i++){
+            chessBoardSquares[6][i].setIcon((wpawn));
+        }
+        chessBoardSquares[0][0].setIcon(brook); chessBoardSquares[7][0].setIcon(wrook);
+        chessBoardSquares[0][1].setIcon(bknight); chessBoardSquares[7][1].setIcon(wknight);
+        chessBoardSquares[0][2].setIcon(bbish); chessBoardSquares[7][2].setIcon(wbish);
+        chessBoardSquares[0][3].setIcon(bqueen); chessBoardSquares[7][3].setIcon(wqueen);
+        chessBoardSquares[0][4].setIcon(bking); chessBoardSquares[7][4].setIcon(wking);
+        chessBoardSquares[0][5].setIcon(bbish); chessBoardSquares[7][5].setIcon(wbish);
+        chessBoardSquares[0][6].setIcon(bknight); chessBoardSquares[7][6].setIcon(wknight);
+        chessBoardSquares[0][7].setIcon(brook); chessBoardSquares[7][7].setIcon(wrook);
+
     }
 //    public void addMouseListener(){
 //        for (int row = 0; row< chessBoardSquares.length;row++){
